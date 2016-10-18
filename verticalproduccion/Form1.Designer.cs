13758c1cb46875d56fb1d2a47be23c08f49d3e39
Hector <hpincheira1@hotmail.com>
2016-10-18 13:25:29 -0300

    iconos, imagenes filosogfia.

--- verticalproduccion/Form1.Designer.cs
+++ verticalproduccion/Form1.Designer.cs
@@ -158,6 +158,7 @@
             this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.menuStrip1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.IsMdiContainer = true;
             this.Name = "frmPrincipal";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
